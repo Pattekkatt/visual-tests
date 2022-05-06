ece658cba6d5bc010af601ae2fc969ee66ee2c7a
alex.r <alex.rykhlyuk@zone3000.net>
2022-05-06 09:58:11 +0300

    second commit

---
+++ src/playwright.spec.ts
@@ -0,0 +1,31 @@
+import { test as base } from "@playwright/test";
+import { PlaywrightVisualRegressionTracker } from "@visual-regression-tracker/agent-playwright";
+const {baseUrl, pages, screenWidth, screenHeight} = require("./../testData.js");
+
+type TestFixtures = {
+  vrt: PlaywrightVisualRegressionTracker;
+};
+const test = base.extend<{}, TestFixtures>({
+  vrt: [
+    async ({ browserName }, use) => {
+      await use(new PlaywrightVisualRegressionTracker(browserName));
+    },
+    { scope: "worker" },
+  ],
+});
+test.use({ viewport: { width: screenWidth, height: screenHeight } });
+test.beforeAll(async ({ vrt }) => {
+  await vrt.start();
+});
+test.afterAll(async ({ vrt }) => {
+  await vrt.stop();
+});
+
+test.describe("Visual test", () => {
+  for(let i = 0; i <= pages.length - 1; i++) {
+  test("Visual test for " + pages[i], async ({ page, vrt }) => {
+
+      await page.goto(baseUrl + pages[i]);
+      await vrt.trackPage(page, pages[i], { screenshotOptions: { fullPage: true }, }); });
+  }
+});
---
+++ testData.js
@@ -0,0 +1,9 @@
+module.exports = {
+    screenWidth: 1600,
+    screenHeight: 700,
+    baseUrl: 'https://hostiq.ua',
+    pages : [
+        "/",
+        "/ukr/",
+    ]
+}
