356d3e97a1048e11480b4f7eb3c55426371a5126
alex.r <alex.rykhlyuk@zone3000.net>
2022-06-03 12:46:22 +0300

    delete dynamic elements

--- tests/visual_tests.ts
+++ tests/visual_tests.ts
@@ -12,9 +12,22 @@ test.describe("Visual test", async () => {
     for (let i = 0; i <= pages.length - 1; i++) {
         test("Visual test for " + pages[i], async ({page, vrt}) => {
             await page.goto(pages[i]);
-            await page.evaluate(() => document.querySelector('.horiz-reviews').remove());
+
+            //deleting zopim from page if present
+            if (await page.evaluate(() => document.querySelectorAll('.zopim').length > 0)) {
+                await page.evaluate(() => {
+                    document.querySelector('.zopim').remove()
+                })
+            }
+
+            //deleting reviews from page if present
+            if (await page.evaluate(() => document.querySelectorAll('.horiz-reviews').length > 0)) {
+                await page.evaluate(() => {
+                    document.querySelector('.horiz-reviews').remove()
+                })
+            }
             await page.waitForLoadState('networkidle');
-            await vrt.trackPage(page, pages[i], {screenshotOptions: {fullPage: true},});
+            await vrt.trackPage(page, pages[i], {screenshotOptions: {fullPage: true}});
         })
     }
 });
